39c00748930339cb861021751be34f52e92b7e62
Martin Oehler <oehler@sim.tu-darmstadt.de>
2022-05-09 21:27:49 +0200

    Added image plugin

--- hector_nodelet_topic_tools/src/relay_nodelets.cpp
+++ hector_nodelet_topic_tools/src/relay_nodelets.cpp
@@ -1,7 +1,10 @@
 #include <nodelet_topic_tools/nodelet_throttle.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Image.h>
 #include <pluginlib/class_list_macros.h>
 
 typedef nodelet_topic_tools::NodeletThrottle<sensor_msgs::PointCloud2> NodeletThrottlePointcloud;
+typedef nodelet_topic_tools::NodeletThrottle<sensor_msgs::Image> NodeletThrottleImage;
 
 PLUGINLIB_EXPORT_CLASS(NodeletThrottlePointcloud, nodelet::Nodelet)
+PLUGINLIB_EXPORT_CLASS(NodeletThrottleImage, nodelet::Nodelet)
